427c882678b8960c99e3598873092e43076b7819
VIKRAM SINGH <145076898+vikramsingh9219251196@users.noreply.github.com>
2024-01-08 03:05:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser=require("cookie-parser")
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://chatifyvsingh.vercel.app",
     credentials: true,
   }));
 
